2ee4bcaeb0f04363a57f06aef51b56bcce9631f2
Mamta <mamtachauhan6386@gmail.com>
2025-12-28 23:02:09 +0530

    added project files

--- frontend/src/landing_page/signup/Login.js
+++ frontend/src/landing_page/signup/Login.js
@@ -31,7 +31,7 @@ function Login() {
         "https://zerodha-project-ztyt.onrender.com/login",
         // "http://localhost:3002/login",
         { email, password },
-        { credentials: true }
+        { withCredentials: true }
       );
 
       if (data.success) {
